809e25ac3f008fa928617bb4fc226eaf2862ae68
FilipOprisan <oprisanfilip@gmail.com>
2018-05-10 21:37:52 +0200

    Implementation of TFIDFCalculator
    Creation of Recommender

---
+++ src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
@@ -0,0 +1,11 @@
+package com.github.dawidstankiewicz.forum.tools;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Recommender {
+	
+	public double threshold;
+	public HashMap<String,List<String>> projects;
+	
+}
