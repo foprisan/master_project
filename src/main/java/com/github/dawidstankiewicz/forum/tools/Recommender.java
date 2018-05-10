58e309bc71c092667dc07efa3696edfc929371a3
FilipOprisan <oprisanfilip@gmail.com>
2018-05-10 21:56:08 +0200

    recommender changes

--- src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
+++ src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
@@ -1,11 +1,44 @@
 package com.github.dawidstankiewicz.forum.tools;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+
+import com.github.dawidstankiewicz.forum.entity.Topic;
 
 public class Recommender {
 	
 	public double threshold;
-	public HashMap<String,List<String>> projects;
+	public HashMap<Topic,List<String>> projects;
+	public HashMap <Topic,Double>  result;
+	public String target;
+	
+	
+	
+	
+	public Recommender(double threshold, HashMap<Topic, List<String>> projects, String target) {
+		super();
+		this.threshold = threshold;
+		this.projects = projects;
+		this.target = target;
+	}
+	public double getThreshold() {
+		return threshold;
+	}
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+	public HashMap<Topic, List<String>> getProjects() {
+		return projects;
+	}
+	public void setProjects(HashMap<Topic, List<String>> projects) {
+		this.projects = projects;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
 	
 }
