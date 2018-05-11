ca89d63e06a265ef38f25023071bf2b4e7facb1b
FilipOprisan <oprisanfilip@gmail.com>
2018-05-11 08:47:54 +0200

    Modified Recommender :
    	-fixed attritubes types
    	-created getRecommendations method

--- src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
+++ src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
@@ -9,36 +9,47 @@ import com.github.dawidstankiewicz.forum.entity.Topic;
 public class Recommender {
 	
 	public double threshold;
-	public HashMap<Topic,List<String>> projects;
-	public HashMap <Topic,Double>  result;
-	public String target;
+	public HashMap<Integer,List<String>> projects;
+	public HashMap <Integer,Double>  result;
+	public List<String> target;
 	
 	
 	
 	
-	public Recommender(double threshold, HashMap<Topic, List<String>> projects, String target) {
+	public Recommender(double threshold, HashMap<Integer, List<String>> projects,
+			List<String> target) {
 		super();
 		this.threshold = threshold;
 		this.projects = projects;
 		this.target = target;
+		this.result=new HashMap<Integer,Double>();
 	}
+	
+	
+
 	public double getThreshold() {
 		return threshold;
 	}
 	public void setThreshold(double threshold) {
 		this.threshold = threshold;
 	}
-	public HashMap<Topic, List<String>> getProjects() {
+	public HashMap<Integer, List<String>> getProjects() {
 		return projects;
 	}
-	public void setProjects(HashMap<Topic, List<String>> projects) {
+	public void setProjects(HashMap<Integer, List<String>> projects) {
 		this.projects = projects;
 	}
-	public String getTarget() {
+	public List<String> getTarget() {
 		return target;
 	}
-	public void setTarget(String target) {
+	public void setTarget(List<String> target) {
 		this.target = target;
 	}
 	
+	
+	public HashMap<Integer,Double> getRecommendations () {
+		
+		
+		return this.result;
+	}
 }
