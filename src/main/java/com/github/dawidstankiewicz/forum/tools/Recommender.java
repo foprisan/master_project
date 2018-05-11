b72e9128c41a05ae1a6dbe5db49def6371c3905b
FilipOprisan <oprisanfilip@gmail.com>
2018-05-11 15:05:33 +0200

    recommender dev

--- src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
+++ src/main/java/com/github/dawidstankiewicz/forum/tools/Recommender.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.github.dawidstankiewicz.forum.entity.Topic;
-
+import com.github.dawidstankiewicz.forum.tools.TFIDFCalculator;
 public class Recommender {
 	
 	public double threshold;
@@ -57,10 +57,21 @@ public class Recommender {
 			ArrayList <String> terms= new ArrayList<String>(projects.get(key));
 			dataset.add(terms);
 		}
-		
+		dataset.add(this.target);
 		//compute tf-idf for each term in target. 
 		
-		for (String term : this.target)
+		//store results : integer : id of the topic, and for each topic, store a hashmap with the term and it s tf-idf
+		HashMap<Integer,HashMap<String,Double>> allResultsForTopicsByTerms =new  HashMap<Integer,HashMap<String,Double>>();
+		for (Integer projectId : projects.keySet()) {
+			HashMap<String,Double> termTFIDF = new HashMap <String,Double>();
+			ArrayList<String> doc= new ArrayList<String>(projects.get(projectId));
+			for (String term:this.target){
+				
+				
+				termTFIDF.put(term,TFIDFCalculator.tfIdf(doc, dataset, term));
+			}
+			allResultsForTopicsByTerms.put(projectId, termTFIDF);
+		}
 		return this.result;
 	}
 }
--- src/main/java/com/github/dawidstankiewicz/forum/tools/TFIDFCalculator.java
+++ src/main/java/com/github/dawidstankiewicz/forum/tools/TFIDFCalculator.java
@@ -8,7 +8,7 @@ public class TFIDFCalculator {
      * @param term String represents a term
      * @return term frequency of term in document
      */
-    public double tf(List<String> doc, String term) {
+    public static double tf(List<String> doc, String term) {
         double result = 0;
         for (String word : doc) {
             if (term.equalsIgnoreCase(word))
@@ -22,7 +22,7 @@ public class TFIDFCalculator {
      * @param term String represents a term
      * @return the inverse term frequency of term in documents
      */
-    public double idf(List<List<String>> docs, String term) {
+    public static double idf(List<List<String>> docs, String term) {
         double n = 0;
         for (List<String> doc : docs) {
             for (String word : doc) {
@@ -41,7 +41,7 @@ public class TFIDFCalculator {
      * @param term term
      * @return the TF-IDF of term
      */
-    public double tfIdf(List<String> doc, List<List<String>> docs, String term) {
+    public static double tfIdf(List<String> doc, List<List<String>> docs, String term) {
         return tf(doc, term) * idf(docs, term);
 
     }
